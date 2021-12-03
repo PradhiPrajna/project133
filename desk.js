87f77702cc0d543c5272276055270e463e6be10b
PradhiPrajna <93035161+PradhiPrajna@users.noreply.github.com>
2021-12-03 16:57:59 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,39 @@
+img = "";
+objects = [];
+status = "";
+function preload() {
+    img = loadImage("desk.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoded);
+    document.getElementById("status").innerHTML = "status_detecting_objects"
+}
+function modelLoded() {
+    console.log("modelLoaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "status:object is detected";
+            fill("#660000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#660000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
